f788e869bdf7fe97252a803b2b651d7a1127106b
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-12 18:08:08 +0200

    12-12-2022 commit

---
+++ AverageWordLength.py
@@ -0,0 +1,12 @@
+import re
+
+
+def calculateAverage(sentence):
+    sentence = re.sub(r'[^a-zA-Z\d\s]', u' ', sentence, flags=re.UNICODE)
+    sentence = re.split(' ', sentence)
+    sentence = list(filter(None, sentence))
+    wordLen = 0
+    for word in sentence:
+        if word != '':
+            wordLen = wordLen + len(word)
+    return round((wordLen / len(sentence)), 2)
